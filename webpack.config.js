693758045893d167ea9323f7e40305de97dabb19
Shawn Inder <shawn@vigour.io>
2017-08-06 18:25:07 -0400

    rename index.html template

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
       },
     }),
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: 'index.template.html',
     }),
     new webpack.HotModuleReplacementPlugin(),
   ],
